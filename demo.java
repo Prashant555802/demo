92304d89c0acd4755446d8d88d8b2317132f7b0b
Prashant555802 <prashantpatil7432@gmail.com>
2025-07-15 16:15:59 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class demo {
             public static void main(String[] args) {
                   
-                  System.out.println("HELLO WORLD Rohan prashant")
+                  System.out.println("HELLO WORLD Rohan prashant");
             }
 }
               
